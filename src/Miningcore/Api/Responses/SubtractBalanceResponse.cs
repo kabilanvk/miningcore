8fcb955892ebce2527810eeacc74d54c9b26a695
anandhiya_baratham <anandhya_baratham@symantec.com>
2022-01-20 09:34:24 -0500

    rename API to /subtractBalance

--- src/Miningcore/Api/Responses/SubtractBalanceResponse.cs
+++ src/Miningcore/Api/Responses/SubtractBalanceResponse.cs
@@ -2,7 +2,7 @@ using Miningcore.Persistence.Model;
 
 namespace Miningcore.Api.Responses
 {
-    public class ResetBalanceResponse
+    public class SubtractBalanceResponse
     {
         public Balance OldBalance { get; set; }
         public Balance NewBalance { get; set; }
