58fe97f4e3bb2b2268e5905cb78ff1c56e99d212
Kabilan VK <kabilan.vk@nortonlifelock.com>
2022-01-21 10:18:53 -0500

    updated script

--- scripts/test.sh
+++ scripts/test.sh
@@ -14,15 +14,13 @@ PERSISTENCE_COSMOS_AUTHORIZATIONKEY=$8
 PERSISTENCE_COSMOS_DATABASEID=$9
 
 echo 'Host: ${PERSISTENCE_POSTGRES_HOST}'
-echo `Host: $PERSISTENCE_POSTGRES_USER`
-echo 'Host: $PERSISTENCE_POSTGRES_PASSWORD'
-echo 'Host: $PERSISTENCE_COSMOS_ENDPOINTURL'
-echo 'Host: $PERSISTENCE_COSMOS_AUTHORIZATIONKEY'
-echo 'Host: $PERSISTENCE_COSMOS_DATABASEID'
+echo 'Host: ${persistence-postgres-host}'
+echo 'Database: ${PERSISTENCE_COSMOS_DATABASEID}'
+echo 'Database: ${persistence-cosmos-databaseId}'
 
 #Dependencies
 sudo apt-get update -y && sudo apt-get install -y --no-install-recommends jq 
 
 #Update config
 cd $SRC_DIR
-jq '.persistence.postgres.host="$PERSISTENCE_POSTGRES_HOST"|.persistence.postgres.user="$PERSISTENCE_POSTGRES_USER"|.persistence.postgres.password="$PERSISTENCE_POSTGRES_PASSWORD"|.persistence.cosmos.endpointUrl="$PERSISTENCE_COSMOS_ENDPOINTURL"|.persistence.cosmos.authorizationKey="$PERSISTENCE_COSMOS_AUTHORIZATIONKEY"|.persistence.cosmos.databaseId="$PERSISTENCE_COSMOS_DATABASEID"' $SRC_DIR/src/Miningcore.Integration.Tests/config_test.json > tmp.$$.json && mv tmp.$$.json $SRC_DIR/src/Miningcore.Integration.Tests/config_test.json
+jq '.persistence.postgres.host="${PERSISTENCE_POSTGRES_HOST}"|.persistence.postgres.user="${PERSISTENCE_POSTGRES_USER}"|.persistence.postgres.password="${PERSISTENCE_POSTGRES_PASSWORD}"|.persistence.cosmos.endpointUrl="${PERSISTENCE_COSMOS_ENDPOINTURL}"|.persistence.cosmos.authorizationKey="${PERSISTENCE_COSMOS_AUTHORIZATIONKEY}"|.persistence.cosmos.databaseId="${PERSISTENCE_COSMOS_DATABASEID}"' $SRC_DIR/src/Miningcore.Integration.Tests/config_test.json > tmp.$$.json && mv tmp.$$.json $SRC_DIR/src/Miningcore.Integration.Tests/config_test.json
